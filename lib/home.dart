1557b86f1f99cc77da449a8841578777c2dc504a
eoeefosa <eoeefosa@gmail.com>
2022-10-10 00:46:20 +0100

    'updated'

--- lib/home.dart
+++ lib/home.dart
@@ -7,26 +7,40 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.popAndPushNamed(context, "/OnStatefullhompage");
-                },
-                child: const Text(
-                  "Statefullhomepage",
-                  textAlign: TextAlign.center,
-                ))
-          ],
-        ),
-      ),
+      theme: ThemeData.light(),
+      home: const Homepager(),
       routes: {
         "/OnStatefullhompage": (context) => const StatefulHomepage(),
         "/OnSimplehomepage": (context) => const Simplehomepage(),
       },
+    );
+  }
+}
+
+class Homepager extends StatelessWidget {
+  const Homepager({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, "/OnStatefullhompage");
+                  },
+                  child: const Text(
+                    "Statefullhomepage",
+                    textAlign: TextAlign.center,
+                  )),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
--- lib/screens/providerhomepage.dart
+++ lib/screens/providerhomepage.dart
@@ -1,7 +1,42 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:get/utils.dart';
 import 'package:provider/provider.dart';
 
-class State extends ChangeNotifier {
+class Providerhomepage extends StatelessWidget {
+  Providerhomepage({super.key});
+  final TextEditingController _controller = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => Stated(),
+      builder: (context, index) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Consumer<Stated>(builder: (context, value, child) => Text("Hello, ${value.name}")),
+          ),
+          body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            TextFormField(controller: _controller),
+            TextButton(
+              onPressed: () => Provider.of<Stated>(context, listen: false).setname(_controller.text),
+              child: const Text('I have entereed my name'),
+            )
+          ]),
+        );
+      },
+    );
+  }
+}
+
+class Stated extends ChangeNotifier with DiagnosticableTreeMixin {
   String name = "john Doe";
+  void setname(String newname) {
+    name = newname;
+    notifyListeners();
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(StringProperty('count', name));
+  }
 }
